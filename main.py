4b91c85762052863da1b4d05521c7605377cdf4f
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-05-30 00:25:40 +0700

    first hand

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
 concept_data = pd.read_csv("data/concepts.csv")
 train_data = pd.read_csv("data/train.csv")
 test_data = pd.read_csv("data/test.csv")
 
-print(train_data.shape)
-print(train_data.isnull().sum())
+#! Data Preprocessing
+
+#! Descriptive Analysis
+
+#! Model Training
+rf_model = RandomForestRegressor(n_estimators=200, random_state=2025)
+
+#! Model Evaluation (MSE and R2)
